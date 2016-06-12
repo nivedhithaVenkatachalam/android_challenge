2bf7fdd98e1df45fb1476da7f6259c88584db3e5
NivedhithaV <nivedhi170589@gmail.com>
2016-06-11 17:19:14 -0700

    Add methods to carousel class

--- app/src/main/java/com/fandor/challenge/Carousel.java
+++ app/src/main/java/com/fandor/challenge/Carousel.java
@@ -2,6 +2,7 @@ package com.fandor.challenge;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Handler;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -10,8 +11,12 @@ import java.util.ArrayList;
  * Created by nivedhithav on 6/11/16.
  */
 public class Carousel {
+    protected static final long TIME_DELAY = 6000;
     ArrayList<String> arr;
     TextView tv;
+    Handler handler=new Handler();
+    int arrSize=0;
+    int count=0;
 
     public Carousel(){
         arr=new ArrayList<String>();
@@ -19,6 +24,7 @@ public class Carousel {
 
     public void setArr(ArrayList<String> arr){
         this.arr=arr;
+        arrSize=arr.size();
     }
 
     public ArrayList<String> getArr(){
@@ -35,5 +41,26 @@ public class Carousel {
 
     public void addIntroDescription(String desc) {
         arr.add(desc);
+        arrSize++;
     }
+
+    public void updateDescContinuously(){
+
+        handler.post(updateTextRunnable);
+    }
+
+    Runnable updateTextRunnable=new Runnable(){
+        public void run() {
+            if(count<arrSize) {
+                tv.setText(arr.get(count));
+                count++;
+            }
+            else{
+                tv.setText(arr.get(0));
+                count=1;
+
+            }
+            handler.postDelayed(this, TIME_DELAY);
+        }
+    };
 }
